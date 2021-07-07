3992d65916ecaccb0d42c113bed89e34d14cce38
IlyaTsaplin <ilyacaplin2015@gmail.com>
2021-07-07 19:49:46 +0300

    #1 Classes for drawing a tree. Window for drawing a tree.

---
+++ drawableTree/src/main/kotlin/DrawableLink.kt
@@ -0,0 +1,11 @@
+import javafx.scene.shape.Line
+
+class DrawableLink(var startX: Double, var startY: Double, var endX: Double, var endY: Double){
+    var line = Line()
+
+    init{
+        line = Line(startX, startY, endX, endY)
+        line.strokeWidth = 3.0
+    }
+
+}
