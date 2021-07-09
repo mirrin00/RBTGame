5666bfa3b2cddf0cb6c0ddd581c83f98d9cbc8f4
IlyaTsaplin <ilyacaplin2015@gmail.com>
2021-07-09 22:46:29 +0300

    #1 Added fixed animations

--- red_black_tree/src/main/kotlin/summer/practice/infty/MyApp.kt
+++ red_black_tree/src/main/kotlin/summer/practice/infty/MyApp.kt
@@ -1,6 +1,4 @@
-package summer.practice.infty
-
 import summer.practice.infty.view.TreeView
 import tornadofx.*
 
-class MyApp: App(TreeView::class, Styles::class)
+class MyApp: App(TreeView::class)
---
+++ red_black_tree/src/main/kotlin/summer/practice/infty/view/TreeView.kt
@@ -0,0 +1,113 @@
+package summer.practice.infty.view
+
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.Group
+import javafx.scene.control.ScrollPane
+import javafx.scene.layout.StackPane
+import tornadofx.*
+
+import summer.practice.infty.controllers.DrawingController
+
+
+class TreeView: View() {
+
+    override val root = vbox {
+        val myController = DrawingController()
+        val input = SimpleStringProperty()
+
+        var mainPane = StackPane()
+        var treeGroup: Group
+
+        val scroll = scrollpane {
+
+            setMinSize(600.0, 750.0)
+
+            hvalue = 0.5
+            vvalue = 0.5
+
+            mainPane = stackpane {
+
+                treeGroup = Group()
+
+                setMinSize(1000000.0, 1000000.0) //TODO: add resize
+                isPannable = true
+                hbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
+                vbarPolicy = ScrollPane.ScrollBarPolicy.NEVER
+
+
+                setOnScroll {
+                    val movement = it.deltaY
+                    var zoomFactor = 1.05
+
+                    if (movement < 0){
+                        zoomFactor = 0.90
+                    }
+
+                    mainPane.scaleX = mainPane.scaleX * zoomFactor
+                    mainPane.scaleY = mainPane.scaleY * zoomFactor
+
+                    it.consume()
+                }
+            }
+
+        }
+
+        textfield(input)
+
+        button {
+            label("Add random node")
+            action {
+
+                myController.addNodeAction()
+                treeGroup = myController.drawTree()
+
+                mainPane.clear()
+                mainPane.add(treeGroup)
+
+                scroll.hvalue = 0.5
+                scroll.vvalue = 0.5
+            }
+        }
+
+        button {
+            label("Remove node")
+            action {
+                try{
+                    myController.removeNodeAction(input.value.toInt())
+                    input.value = ""
+                    treeGroup = myController.drawTree()
+
+                    mainPane.clear()
+                    mainPane.add(treeGroup)
+
+                    scroll.hvalue = 0.5
+                    scroll.vvalue = 0.5
+                }
+                catch (e: Exception){
+
+                }
+            }
+        }
+
+        button {
+            label("Reset tree")
+            action {
+                myController.resetTree()
+
+                treeGroup = myController.drawTree()
+
+                mainPane.clear()
+                mainPane.add(treeGroup)
+
+                scroll.hvalue = 0.5
+                scroll.vvalue = 0.5
+            }
+        }
+
+    }
+}
+
+
+
+
+
