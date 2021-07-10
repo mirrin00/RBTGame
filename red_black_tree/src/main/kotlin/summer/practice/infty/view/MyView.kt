2d4bff325ba069fd46368b0430a77b80e7963c88
aseleznyova <nastya.seleznyovaa@gmail.com>
2021-07-10 19:52:11 +0300

    #2 added map

--- red_black_tree/src/main/kotlin/summer/practice/infty/view/MyView.kt
+++ red_black_tree/src/main/kotlin/summer/practice/infty/view/MyView.kt
@@ -1,31 +1,41 @@
 package summer.practice.infty.view
 
+import javafx.beans.property.SimpleStringProperty
 import summer.practice.infty.controllers.MyController
 import javafx.scene.Group
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.StackPane
+import javafx.scene.control.ScrollPane
+import javafx.scene.control.Button
+import summer.practice.infty.controllers.DrawingController
 import tornadofx.*
 
 
 class MyView: View() {
     override val root : BorderPane by fxml("/MyInterface.fxml")
-    val myController = MyController()
-
-    val mainPane : StackPane by fxid("stac")
-    var treeGroup = Group()
-
+    val RBTmap = MyMap()
+    val butt : Button by fxid("act3")
     fun plusNode() {
-        myController.addNode()
-        treeGroup = myController.drawTree()
-
-        mainPane.clear()
-        mainPane.add(treeGroup)
+        RBTmap.plusNode2()
     }
     fun minusNode() {
-        myController.removeNodeAction()
-        treeGroup = myController.drawTree()
+        RBTmap.minusNode2()
+    }
+    fun resetTree(){
+        RBTmap.resetTree2()
+    }
+    fun OpenTheMap(){
+        RBTmap.openWindow()
+    }
 
-        mainPane.clear()
-        mainPane.add(treeGroup)
+    fun disableAct(){
+        if(!butt.isDisable()){
+            butt.setDisable(true)
+            butt.setVisible(false)
+        }
+        else{
+            butt.setDisable(false)
+            butt.setVisible(true)
+        }
     }
 }
