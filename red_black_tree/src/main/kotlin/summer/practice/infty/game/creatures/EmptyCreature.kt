f5d3126adfef88a872092ad587cc0a8247b65afa
mirrin00 <mirrin1905@outlook.com>
2021-07-10 12:31:10 +0100

    #7 Added ResourceLoader and images

--- red_black_tree/src/main/kotlin/summer/practice/infty/game/creatures/EmptyCreature.kt
+++ red_black_tree/src/main/kotlin/summer/practice/infty/game/creatures/EmptyCreature.kt
@@ -1,7 +1,15 @@
 package summer.practice.infty.game.creatures
 
 import summer.practice.infty.game.Element
+import summer.practice.infty.game.Player
 
 class EmptyCreature: Creature{
     override val element: Element = Element.NONE
+    override val health: Int = 0
+    override val damage: Int = 0
+    override var in_battle: Boolean = false
+
+    override fun Attack(player: Player){}
+
+    override fun getActions(): ArrayList<String> = ArrayList()
 }
