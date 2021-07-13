72ec7a9aa6c022b80553bb673880f0c3c48ad128
aseleznyova <nastya.seleznyovaa@gmail.com>
2021-07-13 11:05:01 +0300

    #10 added interface controller

---
+++ red_black_tree/src/main/kotlin/summer/practice/infty/controllers/InterfaceController.kt
@@ -0,0 +1,29 @@
+package summer.practice.infty.controllers
+
+import summer.practice.infty.actions.Action
+import summer.practice.infty.game.Game
+import summer.practice.infty.view.MyView
+import tornadofx.Controller
+
+class InterfaceController(gameWindow : MyView) : Controller(){
+    var picked : Int = -1
+    val game = Game(gameWindow)
+    fun swapItems(i : Int){
+        if (picked < 0){
+            picked = i
+        }
+        else{
+            game.swapPlayerItems(picked, i)
+            picked = -1
+        }
+    }
+    fun sold(i : Int, active : Boolean){
+        game.sold(i, active)
+    }
+    fun act(a : Action){
+        a.act(game)
+    }
+    fun start(){
+        game.start()
+    }
+}
